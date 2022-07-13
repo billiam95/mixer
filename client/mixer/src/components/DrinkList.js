3362a84decbc31ec34f45e8922750a89ceee9548
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-07-13 11:14:05 -0400

    final merge from 7/12

--- client/mixer/src/components/DrinkList.js
+++ client/mixer/src/components/DrinkList.js
@@ -2,42 +2,13 @@ import { useState, useEffect } from 'react';
 
 const DrinkList = () => {
 
-<<<<<<< HEAD
-  const [ drinkList, setDrinkList ] = useState([])
-=======
   const [ drinkList, setDrinkList ] = useState([]);
   const [ cardSide, setCardSide ] = useState(true);
->>>>>>> cea6af002bd63068dc8d67ed64050fd606bca2e3
 
   const getDrinks = () => {
     fetch('http://localhost:3000/drinks')
     .then(response => response.json())
     .then(data => setDrinkList(data));
-<<<<<<< HEAD
-  }
-
-  const map = drinkList.map((drink) => {
-    return (
-      <div>
-        <h1 className='m-20' key={drink._id}>{drink.name}</h1>
-      </div>
-    )
-  })
-
-  useEffect(() => {
-    getDrinks()
-  }, [])
-
-  return (
-    <main>
-      <button className="border-2 p-3">Add Drink</button>
-      {map}
-    </main>
-  )
-}
-
-export default DrinkList;
-=======
   };
 
   useEffect(() => {
@@ -76,4 +47,3 @@ export default DrinkList;
 }
 
 export default DrinkList;
->>>>>>> cea6af002bd63068dc8d67ed64050fd606bca2e3
--- client/mixer/tailwind.config.js
+++ client/mixer/tailwind.config.js
@@ -1,16 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: [
-    "./src/index.css", 
-    "./src/App.js", 
-    "./src/components/About.js", 
-    "./src/components/NavBar.js", 
-    "./src/components/DrinkList.js"
-  ],
-=======
   content: ["./src/index.css", "./src/App.js", "./src/components/About.js", "./src/components/NavBar.js", "./src/components/DrinkList.js"],
->>>>>>> cea6af002bd63068dc8d67ed64050fd606bca2e3
   theme: {
     extend: {},
   },
